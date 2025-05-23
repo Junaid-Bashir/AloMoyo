7fd63f86c5886f726db9175e2fde04bdd94f1a8a
Junaid Bashir <47460568+Junaid-Bashir@users.noreply.github.com>
2025-05-23 21:09:18 +0200

    updated url

--- app/core/config.py
+++ app/core/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    APP_BASE_URL: str = "http://localhost:8000"
+    APP_BASE_URL: str = "https://api.alokazi.com"
 
     # Backwards-compatible lowercase properties
     @property
